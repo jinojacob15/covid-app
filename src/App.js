bfcb6b0f8444f141d956eecd7db9d13d0b517319
Jino C Jacob <jcjacob@salmonggn.com>
2020-03-30 17:32:37 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import 'assets/main.scss';
 import MainComponent from 'components/MainComponent'
+import Footer from 'components/Footer'
 
 export const  App = () =>  {
   return (
     <div className="App">
       <MainComponent/>
-      
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+	return(
+		 <div className='footer'>
+		 Developed by Jino cj
+      </div>
+		)
+}
+
+export default Footer

4c5826f1c10d892cce39d355bfb6c8e1c35622d6
Jino C Jacob <jcjacob@salmonggn.com>
2020-03-30 16:59:44 +0530

    added other graphs

--- src/components/GraphComponent.js
+++ src/components/GraphComponent.js
@@ -5,41 +5,102 @@ import {Line} from 'react-chartjs-2';
 
  const  GraphComponent = (props) =>  {
 
- 	const [labels,setLabel]=useState([])
- 	const[graphData,setGraphData] = useState([]) 
+ 	const [confirmedLabels,setconfirmedLabels]=useState([])
+ 	const[graphConfirmedData,setgraphConfirmedData] = useState([]) 
+
+ 	
+ 	const[graphDeceasedData,setgraphDeceasedData] = useState([]) 
+
+ 	
+ 	const[graphrecoveredData,setgraphrecoveredData] = useState([]) 
 
  	useEffect(() => {
  		const dailyData = props.dailyStats;
  		const labels = dailyData.map(every => every.date)
  		const graphData = dailyData.map(every => every.dailyconfirmed)
- 		setLabel(labels);
- 		setGraphData(graphData);
+ 		setconfirmedLabels(labels);
+ 		setgraphConfirmedData(graphData);
+ 		setgraphDeceasedData(dailyData.map(every => every.dailydeceased))
+ 		setgraphrecoveredData(dailyData.map(every => every.dailyrecovered))
+
    
   },[props.dailyStats]);
 
-const data = {
-  labels: [...labels],
+const confirmedProps = {
+  labels: [...confirmedLabels],
   datasets: [
     {
-      label: 'cases resported',
+      label: 'confirmed',
       fill: false,
       lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
+      backgroundColor: '#ff1a1a',
+      borderColor: '#ff1a1a',
       borderCapStyle: 'butt',
       borderDash: [],
       borderDashOffset: 0.0,
       borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBorderColor: '#ff1a1a',
       pointBackgroundColor: '#fff',
       pointBorderWidth: 1,
       pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBackgroundColor: '#ff1a1a',
+      pointHoverBorderColor: '#ff1a1a',
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
-      data: [...graphData]
+      data: [...graphConfirmedData]
+    }
+  ]
+};
+const deceasedProps = {
+  labels: [...confirmedLabels],
+  datasets: [
+    {
+      label: 'deaths resported',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: '#808080',
+      borderColor: '#808080',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: '#808080',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: '#808080',
+      pointHoverBorderColor: '#808080',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [...graphDeceasedData]
+    }
+  ]
+};
+const recoveredProps = {
+  labels: [...confirmedLabels],
+  datasets: [
+    {
+      label: 'Daily recoveries',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: '#2eb82e',
+      borderColor: '#2eb82e',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: '#2eb82e',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: '#2eb82e',
+      pointHoverBorderColor: '#2eb82e',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [...graphrecoveredData]
     }
   ]
 };
@@ -47,7 +108,13 @@ const data = {
   return (
     <div className="GraphComponent">
       <Container>
-           <Line data={data} />
+           <Line data={confirmedProps} />
+        </Container>
+         <Container>
+           <Line data={deceasedProps} />
+        </Container>
+         <Container>
+           <Line data={recoveredProps} />
         </Container>
     </div>
   );

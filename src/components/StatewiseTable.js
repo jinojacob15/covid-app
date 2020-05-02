dcc947cb1f82856c0fdcf659e49c783235fa25bb
Jino C Jacob <jcjacob@salmonggn.com>
2020-05-02 16:01:52 +0530

    responsive issues resolved

--- src/components/StatewiseTable.js
+++ src/components/StatewiseTable.js
@@ -4,40 +4,50 @@ import 'react-table-v6/react-table.css'
 import ReactGA from 'react-ga'
 import DistictWiseView from'components/DistictWiseView'
 import PercentageRender from 'components/PercentageRender'
+import {isMobileOnly} from 'react-device-detect';
+
+const MOBILE_WIDTH=55;
+const MOBILE_MAX_WIDTH = undefined;
+const MOBILE_WIDTH_STATE_COL=60
 
 
  const  StatewiseTable = (props) =>  {
 
  	const columns = [{
- 		Header: 'State/UT',
-	    accessor: 'state' ,
-        headerClassName:'sticky'
+ 		Header: isMobileOnly?'S/U':'State/UT',
+		accessor: 'state' ,
+		width:isMobileOnly?MOBILE_WIDTH_STATE_COL:undefined,
+		maxWidth:isMobileOnly?MOBILE_MAX_WIDTH:undefined,
 	  }, {
-	    Header: 'Confirmed',
+	    Header: isMobileOnly?'C':'Confirmed',
 	    id:"Confirmed",
 	    accessor: c => Number(c.confirmed),
-	    className:'text-right',
-	    headerClassName:'sticky',
+		className:'text-right',
+		width:isMobileOnly?MOBILE_WIDTH:undefined,
+		maxWidth:isMobileOnly?MOBILE_MAX_WIDTH:undefined,
 	    Cell: ({original}) => <span><span className="confirmed-delta">{original.deltaconfirmed!=='0'?(<>+{original.deltaconfirmed}</>):('')}</span>{" "}{original.confirmed}</span> 
 	  }, {
-	    Header: 'Active', 
+	    Header: isMobileOnly?'A':'Active', 
 	     id: 'Active', 
-	     accessor: c => Number(c.active),
+		 accessor: c => Number(c.active),
+		 width:isMobileOnly?MOBILE_WIDTH:undefined,
+		 maxWidth:isMobileOnly?MOBILE_MAX_WIDTH:undefined,
 	     className:'text-right',
-	     headerClassName:'sticky'
 	  },{
-	    Header: 'Recovered',
+	    Header: isMobileOnly?'R':'Recovered',
 	    accessor: c => Number(c.recovered),
 	    id:'Recovered',
-	     className:'text-right',
-	     headerClassName:'sticky',
+		 className:'text-right',
+		 width:isMobileOnly?MOBILE_WIDTH:undefined,
+		 maxWidth:isMobileOnly?MOBILE_MAX_WIDTH:undefined,
 	      Cell: ({original}) => <span><PercentageRender select={original.recovered} total={original.confirmed}/>{" "}{" "}{original.recovered}  </span> 
 	  },{
-	    Header: 'Deceased',
+	    Header: isMobileOnly?'D':'Deceased',
 	    id:'Deceased',
 	      accessor: c => Number(c.deaths),
-	     className:'text-right',
-	     headerClassName:'sticky',
+		 className:'text-right',
+		 width:isMobileOnly?MOBILE_WIDTH:undefined,
+		 maxWidth:isMobileOnly?MOBILE_MAX_WIDTH:undefined,
 	     Cell: ({original}) => <span><PercentageRender select={original.deaths} total={original.confirmed}/>{" "}{" "}{original.deaths}   </span> 
 	  }
  	]

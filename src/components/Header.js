9f693bcd8fc56a0790136d47e9b53a9febe8492c
Jino C Jacob <jcjacob@salmonggn.com>
2020-05-02 17:02:42 +0530

    moment removed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,12 @@
 import React  from 'react';
-import moment from 'moment'
 
 
  const  Heading = (props) =>  {
-
-var a = moment(props.lastUpdateTime,'DD-MM-YYYY HH:mm');
-var b = moment();
-
-
 
   return (
     <div className="Heading">
       
            <h4>COVID INDIA TRACKER</h4>
-           <span className="last-update">Last updated {a.from(b)}</span>
-         
          
     </div>
   );

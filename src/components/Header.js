88e0c688124402045ec89903d272bc5e8c0f9b72
Jino C Jacob <jcjacob@salmonggn.com>
2020-05-06 11:44:49 +0530

    last updated added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,13 @@ import React  from 'react';
 
 
  const  Heading = (props) =>  {
+   console.log(props.lastUpdateTime)
 
   return (
     <div className="Heading">
       
            <h4>COVID INDIA TRACKER</h4>
-         
+  <span className="last-update">Last updated  on {props.lastUpdateTime.toString()}</span>
     </div>
   );
 }
